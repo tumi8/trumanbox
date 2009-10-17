fa1e177b2e6c18991d0acf3e808683e9c162b4e6
lobraun <lobraun@2c3c31c4-80a4-42ef-a19d-bbb6a4b49092>
2009-10-17 16:50:34 +0000

    remove unnecessary header files
    git-svn-id: https://trumanbox.svn.sourceforge.net/svnroot/trumanbox@33 2c3c31c4-80a4-42ef-a19d-bbb6a4b49092

--- src/dispatching.c
+++ src/dispatching.c
@@ -1,23 +1,6 @@
-#include <stdio.h>
-#include <netinet/ip.h>
-#include <netinet/tcp.h>
-#include <sys/socket.h>
-#include <unistd.h>
-#include <sys/stat.h>
-#include <fcntl.h>
-#include <stdlib.h>
-#include <errno.h>
-#include <string.h>
-#include <sys/time.h>
-#include <sys/types.h>
-#include <sys/wait.h>
-#include <time.h>
-
-#include "definitions.h"
+#include "dispatching.h"
 #include "signals.h"
 #include "configuration.h"
-
-#include "dispatching.h"
 #include "wrapper.h"
 #include "helper_file.h"
 #include "helper_net.h"
