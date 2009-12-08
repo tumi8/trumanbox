829b23af43446f5adabd927eef779b2b77390eeb
lobraun <lobraun@2c3c31c4-80a4-42ef-a19d-bbb6a4b49092>
2009-12-08 10:37:48 +0000

    added mode to configuration
    git-svn-id: https://trumanbox.svn.sourceforge.net/svnroot/trumanbox@71 2c3c31c4-80a4-42ef-a19d-bbb6a4b49092

--- src/configuration.c
+++ src/configuration.c
@@ -7,12 +7,14 @@
 
 struct configuration_t {
 	dictionary* dict;
+	operation_mode_t mode;
 };
 
 struct configuration_t* conf_create(const char* config_file)
 {
 	struct configuration_t* ret = (struct configuration_t*)malloc(sizeof(struct configuration_t));
 	ret->dict = iniparser_new(config_file);
+	ret->mode = invalid;
 	if (ret->dict == NULL) {
 		msg(MSG_ERROR, "Error parsing configuration file!");
 		goto out1;
@@ -41,5 +43,33 @@ int conf_getint(struct configuration_t* c, const char* module, const char* key, 
 		return notfound;
 	}
 	return atoi(tmp);
+}
+
+operation_mode_t conf_get_mode(struct configuration_t* c)
+{
+	operation_mode_t m = invalid;
+	if (-1 == (m = conf_getint(c, "main", "mode", -1))) {
+		return c->mode;
+	}
+	if (m > invalid && m < quit) {
+		return c->mode;
+	}
+	return invalid;
+}
+
+int conf_set_mode(struct configuration_t* c, operation_mode_t mode)
+{
+	if (-1 == conf_getint(c, "main", "mode", -1)) {
+		// there is no mode in the configuration file
+		if (mode > invalid && mode < quit) {
+			c->mode = mode;
+			return 0;
+		}
+		msg(MSG_ERROR, "Invalid mode given: %d", mode);
+		return -1;
+	} else {
+		msg(MSG_ERROR, "Mode given in configuration file. Cannot overwrite this value!");
+	}
+	return -1;
 }
 
--- src/configuration.h
+++ src/configuration.h
@@ -2,12 +2,33 @@
 #define _CONFIGURATION_H_
 
 #include <stdint.h>
+#include "definitions.h"
 
 struct configuration_t;
 
+/* Creates a configuration object by parsing the given ini file
+ * @returns a configuration object on sucess, NULL otherwise
+ */
 struct configuration_t* conf_create(const char* config_file);
+
 void conf_destroy(struct configuration_t* c);
+
 const char* conf_get(struct configuration_t* c, const char* module, const char* key);
 int conf_getint(struct configuration_t* c, const char* module, const char* key, int notfound); 
+
+/*
+ * Returns the application mode. The return value depends on different program state:
+ * If the mode was specified in the configuration file, this mode will be returned
+ * If no mode was given in the config file, INVALID will be returned as long as there 
+ * was no mode set with @conf_set_mode
+ */
+operation_mode_t conf_get_mode(struct configuration_t* c);
+
+/* Sets the application run mode, if there was no mode set in the config file.
+ * This function will return an error if there was a valid mode in the configuration.
+ * @return 0 on success, 1 on error
+ */
+int conf_set_mode(struct configuration_t* c, operation_mode_t mode);
+
 
 #endif
