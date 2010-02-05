d38585f06b57c0b6bad7bb8ba2f15a538ab68452
lobraun <lobraun@2c3c31c4-80a4-42ef-a19d-bbb6a4b49092>
2010-02-05 15:34:27 +0000

    instanciate log_truman
    git-svn-id: https://trumanbox.svn.sourceforge.net/svnroot/trumanbox@103 2c3c31c4-80a4-42ef-a19d-bbb6a4b49092

--- src/logger.c
+++ src/logger.c
@@ -1,20 +1,45 @@
 #include "logger.h"
 #include "configuration.h"
+#include "msg.h"
+
+#include "log_truman.h"
 
 #include <stdlib.h>
+#include <string.h>
+
 
 struct logger_t* logger_create(struct configuration_t* config)
 {
 	struct logger_t* log = (struct logger_t*)malloc(sizeof(struct logger_t));
 
 	log->config = config;
+
+	// check with logger to instantiate
+	const char* logger = conf_get(config, "logger", "type");
 	
+	if (!strcmp(logger, "truman")) {
+		log->init = lt_init;
+		log->deinit = lt_deinit;
+		log->create_log = lt_create_log;
+		log->finish_log = lt_finish_log;
+		log->log = lt_log_text;
+	} else {
+		msg(MSG_FATAL, "Unknown or not logging subsystem defined in configuration");
+		goto out;
+	}
+
+	log->init(log);
 
 	return log;
+
+out:
+	free(log);
+	return NULL;
 }
 
 void logger_destroy(struct logger_t* logger)
 {
+	logger->deinit(logger);
 	free(logger);
 }
 
--- src/logger.h
+++ src/logger.h
@@ -14,17 +14,30 @@ typedef int (lg_log_text)(struct logger_t*, char* fmt, ...);
 
 enum logger_type { directory };
 
+/**
+ * Note: Every logging module MUST expect concurrent access to the logging
+ * data by several *processes*, as the trumanbox forks several processes 
+ * in order to do its jobs.
+ */
 struct logger_t {
-	void* logger_data;
+	void* data;
 	struct configuration_t* config;
 
-	lg_init* init;
+	lg_init* init; // will be called by logger_create
+	lg_deinit* deinit; // will be called by logger_destroy
 	lg_create_log* create_log;
 	lg_finish_log* finish_log;
 	lg_log_text* log;
 };
 
+
+/**
+ * This creates the logging object. There may be an arbitrary number of logging objets
+ * The logging module *MUST* expect that different *processes* want concurrent access to
+ * the logs. 
+ */
 struct logger_t* logger_create(struct configuration_t* config);
-void logger_destroy(struct logger_t* logger);
+
+void logger_destroy(struct logger_t*);
 
 #endif
