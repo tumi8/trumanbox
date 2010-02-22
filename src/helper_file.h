f596b2f7e421acf53f57fc4a94533ca5c1b3b797
lobraun <lobraun@2c3c31c4-80a4-42ef-a19d-bbb6a4b49092>
2010-02-22 15:57:23 +0000

    remove unnecessary functions
    git-svn-id: https://trumanbox.svn.sourceforge.net/svnroot/trumanbox@115 2c3c31c4-80a4-42ef-a19d-bbb6a4b49092

--- src/helper_file.h
+++ src/helper_file.h
@@ -8,9 +8,5 @@ void change_to_tmp_folder();
 int create_index_file();
 int create_path_tree(char *path, int user_id, int group_id);
 void build_tree(const connection_t *conn, char *cmd_str);
-void append_to_file(char *str, const connection_t *connection, char *base_dir);
-int write_to_nonexisting_file(char *str, const connection_t *connection, char *base_dir);
-int write_to_file(char *str, char *filename, char *base_dir);
-void creat_file(const connection_t *connection, char *base_dir);
 
 #endif
