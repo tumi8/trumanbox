fcd896cf7e0a64ff3be2d60c917e69aee5ae6c2f
lobraun <lobraun@2c3c31c4-80a4-42ef-a19d-bbb6a4b49092>
2010-03-12 16:27:56 +0000

    check if database exists on program satrt
    git-svn-id: https://trumanbox.svn.sourceforge.net/svnroot/trumanbox@151 2c3c31c4-80a4-42ef-a19d-bbb6a4b49092

--- src/log_sqlite.c
+++ src/log_sqlite.c
@@ -3,6 +3,11 @@
 #include "msg.h"
 
 #include <stdlib.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
 
 #include <sqlite3.h>
 
@@ -11,14 +16,30 @@ struct lsq_data {
 	sqlite3* db;
 };
 
+int create_db(sqlite3* db)
+{
+	return 0;
+}
+
 int lsq_init(struct logger_t* logger)
 {
 	struct lsq_data* data = (struct lsq_data*)malloc(sizeof(struct lsq_data));
+	struct stat buf;
+	int new_db = 0;
 
 	data->db_file = conf_get(logger->config, "logging", "db_file");
 	if (!data->db_file) {
 		msg(MSG_FATAL, "No db_file given in configuration file!");
 		goto out1;
+	}
+	if (-1 == stat(data->db_file, &buf)) {
+		if (errno == ENOENT) {
+			msg(MSG_DEBUG, "Database %s does not exist. Will be created during startup!", data->db_file);
+			new_db = 1;
+		} else {
+			msg(MSG_FATAL, "Error stating database %s: %s", data->db_file, strerror(errno));
+			goto out1;
+		}
 	}
 
 	if ( sqlite3_open(data->db_file, &data->db)) {
