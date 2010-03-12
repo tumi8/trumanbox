4a0cfc6d2e83027f09a4cc8c925d9fe6cf6601cf
lobraun <lobraun@2c3c31c4-80a4-42ef-a19d-bbb6a4b49092>
2010-03-12 16:28:23 +0000

    more sqlite logging infrastructure
    git-svn-id: https://trumanbox.svn.sourceforge.net/svnroot/trumanbox@154 2c3c31c4-80a4-42ef-a19d-bbb6a4b49092

--- src/log_sqlite.c
+++ src/log_sqlite.c
@@ -1,6 +1,7 @@
 #include "log_sqlite.h"
 #include "configuration.h"
 #include "msg.h"
+#include "definitions.h"
 
 #include <stdlib.h>
 #include <sys/types.h>
@@ -8,13 +9,21 @@
 #include <unistd.h>
 #include <errno.h>
 #include <string.h>
+#include <stdio.h>
 
 #include <sqlite3.h>
+
+#define TABLE_NAME_LENGTH 100
+#define MAX_STATEMENT 1000
+
+static char statement[MAX_STATEMENT];
 
 struct lsq_data {
 	const char* db_file;
 	sqlite3* db;
+	char tables[UNKNOWN + 1][TABLE_NAME_LENGTH];
 };
+
 
 static int callback(void *NotUsed, int argc, char **argv, char **azColName){
   int i;
@@ -27,42 +36,58 @@ static int callback(void *NotUsed, int argc, char **argv, char **azColName){
 }
 
 
-int create_db(sqlite3* db)
+int create_db(struct lsq_data* data)
 {
 	char* err = 0;
 	int rc;
+	int i;
 
-	rc = sqlite3_exec(db, "CREATE TABLE currentdns (domain TEXT, original INTEGER, returned INTEGER);", callback, 0, &err);
-	if (rc != SQLITE_OK) {
-		msg(MSG_ERROR, "Error createing table currentdns: %s", err);
-		return -1;
+	for (i = 0; i <= UNKNOWN; ++i) {
+		snprintf(statement, MAX_STATEMENT, "DROP TABLE %s;", data->tables[i]);
+		rc = sqlite3_exec(data->db, statement, callback, 0, &err);
+		//if (rc != SQLITE_OK) {
+		//	msg(MSG_ERROR, "Cannot drop table %s: %s", data->tables[i], err);
+		//	return -1;
+		//}
 	}
 
-	rc = sqlite3_exec(db, "CREATE TABLE currenthttp (domain TEXT, query STRING);", callback, 0, &err);
+	snprintf(statement, MAX_STATEMENT, "CREATE TABLE %s (domain TEXT, original INTEGER, returned INTEGER);", data->tables[DNS]);
+	rc = sqlite3_exec(data->db, statement, callback, 0, &err);
+        if (rc != SQLITE_OK) {
+                msg(MSG_ERROR, "Error createing table currentdns: %s", err);
+                return -1;
+        }
+
+	snprintf(statement, MAX_STATEMENT, "CREATE TABLE %s (domain TEXT, query STRING);", data->tables[HTTP]);
+	rc = sqlite3_exec(data->db, statement, callback, 0, &err);
 	if (rc != SQLITE_OK) {
 		msg(MSG_ERROR, "Error createing table currenthttp: %s", err);
 		return -1;
 	}
 
-	rc = sqlite3_exec(db, "CREATE TABLE currentftp (domain TEXT);", callback, 0, &err);
+	snprintf(statement, MAX_STATEMENT, "CREATE TABLE %s (domain TEXT);", data->tables[FTP]);
+	rc = sqlite3_exec(data->db, statement, callback, 0, &err);
 	if (rc != SQLITE_OK) {
 		msg(MSG_ERROR, "Error createing table currentftp: %s", err);
 		return -1;
 	}
 
-	rc = sqlite3_exec(db, "CREATE TABLE currentsmtp(server TEXT, rctp TEXT);", callback, 0, &err);
+	snprintf(statement, MAX_STATEMENT, "CREATE TABLE %s (server TEXT, rctp TEXT);", data->tables[SMTP]);
+	rc = sqlite3_exec(data->db, statement, callback, 0, &err);
 	if (rc != SQLITE_OK) {
 		msg(MSG_ERROR, "Error createing table currentsmtp: %s", err);
 		return -1;
 	}
 
-	rc = sqlite3_exec(db, "CREATE TABLE currentirc (server TEXT, keyword TEXT, value TEXT);", callback, 0, &err);
+	snprintf(statement, MAX_STATEMENT, "CREATE TABLE %s (server TEXT, keyword TEXT, value TEXT);", data->tables[IRC]);
+	rc = sqlite3_exec(data->db, statement, callback, 0, &err);
 	if (rc != SQLITE_OK) {
 		msg(MSG_ERROR, "Error createing table currentirc: %s", err);
 		return -1;
 	}
 
-	rc = sqlite3_exec(db, "CREATE TABLE currentunknown (content TEXT);", callback, 0, &err);
+	snprintf(statement, MAX_STATEMENT, "CREATE TABLE %s (content TEXT);", data->tables[UNKNOWN]);
+	rc = sqlite3_exec(data->db, statement, callback, 0, &err);
 	if (rc != SQLITE_OK) {
 		msg(MSG_ERROR, "Error createing table currentunknown: %s", err);
 		return -1;
@@ -75,6 +100,15 @@ int lsq_init(struct logger_t* logger)
 	struct lsq_data* data = (struct lsq_data*)malloc(sizeof(struct lsq_data));
 	struct stat buf;
 	int new_db = 0;
+
+	strncpy(data->tables[SMTP], "currentsmtp", TABLE_NAME_LENGTH);
+	strncpy(data->tables[FTP], "currentftp", TABLE_NAME_LENGTH);
+	strncpy(data->tables[FTP_anonym], "currentftp", TABLE_NAME_LENGTH);
+	strncpy(data->tables[FTP_data], "currentftp", TABLE_NAME_LENGTH);
+	strncpy(data->tables[HTTP], "currenthttp", TABLE_NAME_LENGTH);
+	strncpy(data->tables[IRC], "currentirc", TABLE_NAME_LENGTH);
+	strncpy(data->tables[DNS], "currentdns", TABLE_NAME_LENGTH);
+	strncpy(data->tables[UNKNOWN], "currentunknown", TABLE_NAME_LENGTH);
 
 	data->db_file = conf_get(logger->config, "logging", "db_file");
 	if (!data->db_file) {
@@ -96,9 +130,10 @@ int lsq_init(struct logger_t* logger)
 		goto out2;
 	}
 
-	if (new_db) {
-		create_db(data->db);
-	}
+	// TODO: create permanent tables
+	//if (new_db) {
+	//	create_db(data->db);
+	//}
 
 	logger->data = (void*) data;
 	return 0;
@@ -121,8 +156,8 @@ int lsq_deinit(struct logger_t* logger)
 
 int lsq_create_log(struct logger_t* logger)
 {
-	//struct lsq_data* data = (struct lsq_data*)logger->data;
-	return 0;
+	struct lsq_data* data = (struct lsq_data*)logger->data;
+	return create_db(data);
 }
 
 int lsq_finish_log(struct logger_t* logger)
@@ -133,7 +168,33 @@ int lsq_finish_log(struct logger_t* logger)
 
 int lsq_log_text(struct logger_t* logger, connection_t* conn, const char* tag, const char* message)
 {
-	//struct lsq_data* data = (struct lsq_data*)logger->data;
+	struct lsq_data* data = (struct lsq_data*)logger->data;
+	char* err;
+
+	switch (conn->app_proto) {
+	case SMTP:
+		snprintf(statement, MAX_STATEMENT, "INSERT into %s (%s %s);", data->tables[SMTP], conn->orig_dest, message);
+		sqlite3_exec(data->db, statement, callback, 0, &err);
+		break;
+	case FTP:
+	case FTP_anonym:
+	case FTP_data:
+
+		break;
+	case HTTP:
+		
+		break;
+	case IRC:
+
+		break;
+	case DNS:
+
+		break;
+
+	case UNKNOWN:
+		
+		break;
+	};
 	return 0;
 }
 
