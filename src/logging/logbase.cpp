034fc147b9ddd5890be228a438d9795ad74721d3
Lothar Braun <braun@net.in.tum.de>
2011-05-17 14:09:54 +0200

    - started logging library - ported logger to LogBase class

---
+++ src/logging/logbase.cpp
@@ -0,0 +1,54 @@
+#include "logbase.h"
+
+#include <stdlib.h>
+#include <string.h>
+
+#include <common/msg.h>
+#include <common/configuration.h>
+
+LogBase* global_logger = NULL;
+
+int logger_create(const Configuration& config)
+{
+	if (global_logger) {
+		THROWEXCEPTION("Logging module has already been created!");
+	}
+
+	// check with logger to instantiate
+	std::string logger_type = config.get("logging", "type");
+	if (logger_type == "") {
+		THROWEXCEPTION("No logger type given in configuration file!");
+	}
+	
+	if (logger_type != "truman") {
+		// TODO: Create logging object
+		THROWEXCEPTION("The old trumanbox logger is currently not avaialbe ...");
+	} else if (logger_type != "sqlite") {
+		// TODO : create logging object
+		THROWEXCEPTION("The old sqlite logger is currently not available ...");
+	} 
+	 else if (logger_type != "postgresql") {
+		THROWEXCEPTION("The postgresql logger is currently not avaialbe ...");
+	} 
+	else {
+		THROWEXCEPTION("Unknown subsystem defined in configuration. Maybe even undefined!");
+	}
+
+	return 0;
+}
+
+inline LogBase* logger_get()
+{
+	return global_logger;
+}
+
+int logger_destroy()
+{
+	if (!global_logger) {
+		THROWEXCEPTION("Cannot destroy logging module as it has not been created");
+	}
+	delete global_logger;
+	
+	return 0;
+}
+
--- src/logging/logbase.h
+++ src/logging/logbase.h
@@ -1,35 +1,22 @@
-#ifndef _LOGGER_H_
-#define _LOGGER_H_
+#ifndef _LOGBASE_H_
+#define _LOGBASE_H_
 
 #include <common/definitions.h>
 
-struct logger_t;
-struct configuration_t;
+class Configuration;
 
-typedef int (lg_init)(struct logger_t*); // initializes the logging module. Returns 0 on sucess, a value < 0 otherwise
-typedef int (lg_deinit)(struct logger_t*);// cleans up the logger module. Returns 0 on sucess, a value < 0 otherwise
-typedef int (lg_create_log)(struct logger_t*);
-typedef int (lg_finish_log)(struct logger_t*);
-typedef int (lg_log_text)(struct logger_t*, connection_t* conn, const char* tag, const char* message);
-typedef int (lg_log_struct)(struct logger_t*, connection_t* conn, const char* tag, void* data);
-
-enum logger_type { directory };
 
 /**
  * Note: Every logging module MUST expect concurrent access to the logging
  * data by several *processes*, as the trumanbox forks several processes 
  * in order to do its jobs.
  */
-struct logger_t {
-	void* data;
-	struct configuration_t* config;
-
-	lg_init* init; // will be called by logger_create
-	lg_deinit* deinit; // will be called by logger_destroy
-	lg_create_log* create_log;
-	lg_finish_log* finish_log;
-	lg_log_text* log;
-	lg_log_struct* log_struct;
+class LogBase {
+public:
+	LogBase(const Configuration& config);
+	~LogBase();
+protected:
+	const Configuration& config;
 };
 
 
@@ -104,18 +91,20 @@ struct dns_struct {
 
 
 /**
- * This creates the logging object. There may be an arbitrary number of logging objets
+ * This creates the global logging object. There may be an arbitrary number of logging objets
  * The logging module *MUST* expect that different *processes* and modules want concurrent 
  * access to the logs. 
  */
-int logger_create(struct configuration_t* config);
+int logger_create(const Configuration& config);
 
 /**
  * Get the global logger object. logger_create MUST be called once before 
  * this function is called.
  */
-inline struct logger_t* logger_get();
+inline LogBase* logger_get();
 
 int logger_destroy();
+
+
 
 #endif
