bcc31a63798751effc06223123bb998320dbc057
Lothar Braun <braun@net.in.tum.de>
2011-05-17 09:11:42 +0200

    turned configuration module into c++ class

--- src/common/configuration.h
+++ src/common/configuration.h
@@ -2,33 +2,23 @@
 #define _CONFIGURATION_H_
 
 #include <stdint.h>
+#include <string>
 #include "definitions.h"
+#include "iniparser.h"
 
-struct configuration_t;
+class Configuration
+{
+	public:
+		Configuration(const std::string& filename);
+		~Configuration();
 
-/* Creates a configuration object by parsing the given ini file
- * @returns a configuration object on sucess, NULL otherwise
- */
-struct configuration_t* conf_create(const char* config_file);
+		std::string get(const std::string& section, const std::string& key);
 
-void conf_destroy(struct configuration_t* c);
+		operation_mode_t getMode();
 
-const char* conf_get(struct configuration_t* c, const char* module, const char* key);
-int conf_getint(struct configuration_t* c, const char* module, const char* key, int notfound); 
-
-/*
- * Returns the application mode. The return value depends on different program state:
- * If the mode was specified in the configuration file, this mode will be returned
- * If no mode was given in the config file, INVALID will be returned as long as there 
- * was no mode set with @conf_set_mode
- */
-operation_mode_t conf_get_mode(struct configuration_t* c);
-
-/* Sets the application run mode, if there was no mode set in the config file.
- * This function will return an error if there was a valid mode in the configuration.
- * @return 0 on success, 1 on error
- */
-int conf_set_mode(struct configuration_t* c, operation_mode_t mode);
-
+	private:
+		operation_mode_t mode;
+		dictionary* dict;
+};
 
 #endif
