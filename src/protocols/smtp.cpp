50e9944d14530d39d88fc5f455821591b033e6c6
Lothar Braun <braun@net.in.tum.de>
2011-05-17 16:09:35 +0200

    ported SMTP handlers

--- src/protocols/smtp.cpp
+++ src/protocols/smtp.cpp
@@ -1,41 +1,19 @@
 #include "smtp.h"
 #include "wrapper.h"
-#include "logger.h"
-#include "msg.h"
-
 #include <stdlib.h>
 #include <string.h>
 
+#include <logging/logbase.h>
+#include <common/msg.h>
+#include <common/configuration.h>
 
-struct ph_smtp {
-	struct configuration_t* config;
-};
-
-void* ph_smtp_create()
+SMTPHandler::SMTPHandler(const Configuration& config)
+	: ProtoHandler(config)
 {
-	void* ret = malloc(sizeof(struct ph_smtp));
-	return ret;
+
 }
 
-int ph_smtp_destroy(void* handler)
-{
-	free(handler);
-	return 0;
-}
-
-int ph_smtp_init(void* handler, struct configuration_t* c)
-{
-	struct ph_smtp* smtp = (struct ph_smtp*)handler;
-	smtp->config = c;
-	return 0;
-}
-
-int ph_smtp_deinit(void* handler)
-{
-	return 0;
-}
-
-int ph_smtp_handle_payload_stc(void* handler, connection_t* conn, const char* payload, ssize_t* len)
+int SMTPHandler::payloadServerToClient(connection_t* conn, const char* payload, ssize_t* len)
 {
 	char msgCopy[MAXLINE];
 	char* linePtr = NULL;
@@ -56,13 +34,13 @@ int ph_smtp_handle_payload_stc(void* handler, connection_t* conn, const char* pa
 		int Msglength = strcspn(linePtr,"\r\n");
 		strncpy(data->Message,linePtr,Msglength);
 		linePtr = strtok(NULL,"\n");
-		if (logger_get()->log_struct(logger_get(), conn, "server", data)) ;
+		logger_get()->logStruct(conn, "server", data);
 	}
 
 	return 1;
 }
 
-int ph_smtp_handle_payload_cts(void* handler, connection_t* conn, const char* payload, ssize_t* len)
+int SMTPHandler::payloadClientToServer(connection_t* conn, const char* payload, ssize_t* len)
 {
 	struct smtp_struct* data = (struct smtp_struct*) malloc(sizeof(struct smtp_struct));
 	
@@ -81,22 +59,16 @@ int ph_smtp_handle_payload_cts(void* handler, connection_t* conn, const char* pa
 			*len = strlen(payload);
 	}
 	
-	logger_get()->log_struct(logger_get(), conn, "client", data);
+	logger_get()->logStruct(conn, "client", data);
 	return 1;
 }
 
-int ph_smtp_handle_packet(void* handler, const char* packet, ssize_t len)
+int SMTPHandler::determineTarget(struct sockaddr_in* addr)
 {
-	return 0;
-}
-
-int ph_smtp_determine_target(void* handler, struct sockaddr_in* addr)
-{
-	struct ph_smtp* smtp = (struct ph_smtp*)handler;
-	if (conf_get_mode(smtp->config) < full_proxy) {
+	if (config.getMode() < full_proxy) {
                 bzero(addr, sizeof(struct sockaddr_in));
                 addr->sin_family = AF_INET;
-                Inet_pton(AF_INET, conf_get(smtp->config, "smtp", "smtp_redirect"), &addr->sin_addr);
+                Inet_pton(AF_INET, config.get("smtp", "smtp_redirect").c_str(), &addr->sin_addr);
 		addr->sin_port = htons((uint16_t)25);
 	}
 	return 0;
--- src/protocols/smtp.h
+++ src/protocols/smtp.h
@@ -1,8 +1,9 @@
 #ifndef _PROTO_SMTP_H_
 #define _PROTO_SMTP_H_
 
-#include "configuration.h"
 #include <netinet/in.h>
+
+#include "proto_handler.h"
 
 class SMTPHandler : public ProtoHandler
 {
@@ -11,19 +12,6 @@ class SMTPHandler : public ProtoHandler
 		virtual int payloadServerToClient(connection_t* conn, const char* payload, ssize_t* len);
 		virtual int payloadClientToServer(connection_t* conn, const char* payload, ssize_t* len);
 		virtual int determineTarget(struct sockaddr_in* addr);
-
-
 };
-
-
-void* ph_smtp_create();
-int ph_smtp_destroy(void*);
-
-int ph_smtp_init(void* handler, struct configuration_t* c);
-int ph_smtp_deinit(void* handler);
-int ph_smtp_handle_payload_stc(void* handler, connection_t* conn, const char* payload, ssize_t* len);
-int ph_smtp_handle_payload_cts(void* handler, connection_t* conn, const char* payload, ssize_t* len);
-int ph_smtp_handle_packet(void* handler, const char* packet, ssize_t len);
-int ph_smtp_determine_target(void* handler, struct sockaddr_in*);
 
 #endif
